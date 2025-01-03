e1be90dc77c92d6625be8b014ca2b4f91749fca5
v370r <studyrockers5@gmail.com>
2025-01-02 20:36:55 -0700

    changed image and abackground . Added github and logo

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,10 +13,12 @@ export class HomeComponent {
   typed!: Typed;
 
   socialMediaLinks = [
+    { icon: 'bi-linkedin', url: 'https://www.linkedin.com/in/mrvijay-poloju/' },
+    { icon: 'bi-github', url: 'https://github.com/v370r'},
     { icon: 'bi-facebook', url: 'https://www.facebook.com/vector.vijay/' },
     { icon: 'bi-instagram', url: 'https://www.instagram.com/mrvijay.poloju/' },
-    { icon: 'bi-whatsapp', url: 'https://wa.link/tosa3f' },
-    { icon: 'bi-linkedin', url: 'https://www.linkedin.com/in/mrvijay-poloju/' }
+    { icon: 'bi-whatsapp', url: 'https://wa.link/stkb94' },
+
   ];
   constructor(private titleService: Title) {
     this.titleService.setTitle('Vijay - Home')
